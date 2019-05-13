211720682c7a72639d99561dcb603e56609b5a11
PYN <yxp7531540@gmail.com>
2019-05-13 14:22:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+let append = ["Milo", "Otis", "Garfield"]
+append.push("Odie")
+
+let prepend = ["Milo", "Otis", "Garfield"]
+prepend.unshift("Odie")
+
+let removeLast = ["Milo", "Otis", "Garfield"]
+removeLast.pop()
+
+let removeFirst = ["Milo", "Otis", "Garfield"]
+removeFirst.shift()
